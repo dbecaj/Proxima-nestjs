60ed9f9567252c1b1bf5f07e3d0a401104916187
db8349 <db8349@student.uni-lj.si>
2019-07-05 14:55:46 +0200

    Created framework of the REST requests and CRUD operations

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,53 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { ProductDto } from './dto/product.dto';
+import { ProductsService } from './products.service';
+import { Product } from './interfaces/product.interface';
+
+/*
+// Returns a list of products
+router.get('/products', productController.productsReadList);
+
+// Creates a product
+router.post('/product', productController.productCreatePost);
+
+// Returns a product with the specified id
+router.get('/product/:id', productController.productReadGet);
+
+// Removes the product with the specified id
+router.delete('/product/:id', productController.productDelete);
+
+// Updates the product with the specified id
+router.put('/product/:id', productController.productUpdatePost);
+*/
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {}
+
+    // Returns a list of products
+    @Get()
+    async getAll(): Promise<Product[]> {
+        return this.productsService.getAll();
+    }
+
+    // Creates a product
+    @Post()
+    async create(@Body() productDto: ProductDto) {
+        this.productsService.create(productDto);
+    }
+
+    @Get(":id")
+    async get(@Param("id") id): Promise<Product> {
+        return this.productsService.get(id);
+    }
+
+    @Delete(":id")
+    async delete(@Param("id") id): Promise<Product> {
+        return this.productsService.delete(id);
+    }
+
+    @Put(":id")
+    async update(@Param("id") id): Promise<Product> {
+        return this.update(id);
+    }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Product } from './interfaces/product.interface';
+import { ProductDto } from './dto/product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(@Inject('PRODUCT_MODEL') private readonly productModel: Model<Product>) {}
+
+    // Returns a list of products
+    async getAll(): Promise<Product[]> {
+        throw new Error("Not implemented!");
+    }
+
+    // Creates a product
+    async create(productDto: ProductDto): Promise<Product> {
+        throw new Error("Not implemented!");
+    }
+
+    // Returns a product with the specified id
+    async get(id: string): Promise<Product> {
+        throw new Error("Not implemented!");
+    }
+
+    // Removes the product with the specified id
+    async delete(id: string): Promise<Product> {
+        throw new Error("Not implemented!");
+    }
+
+    // Updates the product with the specified id
+    async update(id: string): Promise<Product> {
+        throw new Error("Not implemented!");
+    }
+}
